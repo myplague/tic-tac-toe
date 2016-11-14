dfae09da6533c14a34fb66f603a45b3f9fbf5c76
Swolebrain <thecodingteacher@gmail.com>
2016-11-14 17:28:00 -0500

    feat: empty board with no programming

---
+++ README.md
@@ -0,0 +1,3 @@
+#Build a Tic Tac Toe Game
+
+Take the project that was given and Program the JavaScript so that the Tic Tac Toe board is functional. Whenever a cell is clicked, it should be filled with either an X or an O.
---
+++ main.js
@@ -0,0 +1,33 @@
+(function() {
+    console.log("drawing board");
+    var c = document.getElementById("myCanvas");
+    var canvas = c.getContext("2d");
+    canvas.lineWidth = 1;
+    canvas.strokeStyle = "#fff";
+    //vertical lines
+    canvas.beginPath();
+    canvas.moveTo(100, 0);
+    canvas.lineTo(100, 146.5);
+    canvas.closePath();
+    canvas.stroke();
+    canvas.beginPath();
+    canvas.moveTo(200, 0);
+    canvas.lineTo(200, 146.5);
+    canvas.closePath();
+    canvas.stroke();
+
+    // horizontal lines
+    canvas.lineWidth = .5;
+
+    canvas.beginPath();
+    canvas.moveTo(4, 48.5);
+    canvas.lineTo(296, 48.5);
+    canvas.closePath();
+    canvas.stroke();
+
+    canvas.beginPath();
+    canvas.moveTo(4, 98.5);
+    canvas.lineTo(296, 98.5);
+    canvas.closePath();
+    canvas.stroke();
+  })();
